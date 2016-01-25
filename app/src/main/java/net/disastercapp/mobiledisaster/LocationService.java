5a4905f64719932c5e71beb932ee2ac7e0d8c527
mdj20 <mdj20@pitt.edu>
2016-01-25 13:32:23 -0500

    changes to LocationService and MainActivity

--- app/src/main/java/net/disastercapp/mobiledisaster/LocationService.java
+++ app/src/main/java/net/disastercapp/mobiledisaster/LocationService.java
@@ -8,18 +8,21 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.location.LocationProvider;
+import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.IBinder;
 import android.widget.Toast;
 
 /* Bound service that will run in the primary thread. Gets the users location via location manager.
  */
-
 public class LocationService extends Service {
 
-    private final IBinder lBinder = new LocalBinder(); // binder for bound tasks
+    private final IBinder localBinder = new LocalBinder(); // binder for bound tasks
     private LocationManager locationManager;
     private LocationProvider locationProvider;
+    protected boolean ready = false;
+    protected Location location = null;
+    private boolean isBound = false;
 
     public LocationService() {
         super();
@@ -28,36 +31,74 @@ public class LocationService extends Service {
     @Override
     public void onCreate(){
         Context context = getApplicationContext();
-        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-        locationProvider = locationManager.getProvider(locationManager.getBestProvider(criteria, false));
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);  // prepares locationManager
     }
-
-    // will attempt to get the location from the LocationManager
-    public Location getLocation(){
-        Location location = null;
-        try{
-            location = locationManager.getLastKnownLocation(locationProvider.getName());
-        }
-        catch (SecurityException se){  // if security permission is not set, it will just display a message to user
-            Context context = getApplicationContext();
-            Toast errorToast = Toast.makeText(context,"Bad Location Permissions", Toast.LENGTH_SHORT);
-            errorToast.show();
-        }
-        return location;
-    }
-
 
     @Override
-    public IBinder onBind(Intent intent) {
-        return lBinder;
+    public IBinder onBind(Intent intent) { // returns LocalS
+        isBound = true;
+        return localBinder;
     }
 
-    // binder class
+    @Override
+    public boolean onUnbind(Intent intent){
+        isBound = false;
+        return  isBound;
+    }
+
+    // Simple binder class
     public class LocalBinder extends Binder {
         LocationService getService(){        // method will return LocationServie as an object.
             return LocationService.this;
+        }  // get service returns
+    }
+
+    // will create an AsyncTask that will get the
+    public void findLocation(){
+        new LocationAsyncTask().execute();
+    }
+
+    public boolean ready(){ return ready;}
+
+    //returns location if ready, else returns null
+    public Location getLocation(){
+        if (ready) {return location;}
+        else {return null;}
+    }
+
+    private class LocationAsyncTask extends AsyncTask<String,Boolean,Location> {
+
+        protected Location doInBackground(String[] args){
+            System.out.println("This");
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+            locationProvider = locationManager.getProvider(locationManager.getBestProvider(criteria, true));
+
+            try{
+                location = locationManager.getLastKnownLocation(locationProvider.getName());
+            }
+            catch (SecurityException se){  // if security permission is not set, it will just display a message to user
+                se.printStackTrace();
+                Context context = getApplicationContext();
+                Toast errorToast = Toast.makeText(context,"Bad Location Permissions", Toast.LENGTH_SHORT);
+                errorToast.show();
+            }
+            System.out.println(location.getLatitude());
+
+            return location;
+        }
+
+        protected void onProgressUpdate(Boolean b){
+            // not called or implemented
+        }
+
+        protected void onPostExecute(Location result){
+
+            if (result!=null) {
+                LocationService.this.location = result;
+                ready = true;
+            }
         }
     }
+
 }
--- app/src/main/java/net/disastercapp/mobiledisaster/MainActivity.java
+++ app/src/main/java/net/disastercapp/mobiledisaster/MainActivity.java
@@ -1,40 +1,86 @@
 package net.disastercapp.mobiledisaster;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.location.Location;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.ServiceConfigurationError;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    Location location ;
-
+    Location location;
+    LocationService locationService;
+    boolean locationServiceConnected = false;
+    private ServiceConnection locationServiceConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationServiceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                locationServiceConnected = true;
+                LocationService.LocalBinder localBinder = (LocationService.LocalBinder) service;
+                locationService = localBinder.getService();
+            }
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                locationServiceConnected = false;
+            }
+        };
+        connectLocationService();
+    }
 
+    private void connectLocationService(){
+        if (!locationServiceConnected) {
+            Intent locationServiceIntent = new Intent(this, LocationService.class);
+            bindService(locationServiceIntent,locationServiceConnection, Context.BIND_AUTO_CREATE);
+        }
+    }
 
+    private void setText(TextView textView, String text){
 
     }
 
-    private void launchLocationService(View view ){
+    public void setAsyncLocation(View view){
+        if (!locationServiceConnected){
+            connectLocationService();
+        }
+        setName(view,"setAsync");
+        locationService.findLocation();
+    }
 
+    public void updateLocation(View view){
+        if (locationServiceConnected && locationService.ready() ){
+            location = locationService.getLocation();
+            TextView latitudeTextView = (TextView)findViewById(R.id.latitudeTextView);
+            TextView longitudeTextView = (TextView)findViewById(R.id.longitudeTextView);
 
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+
+                latitudeTextView.setText(Double.toString(latitude));
+                longitudeTextView.setText(Double.toString(longitude));
+
+                setName(view, "NULL");
+
+        }else{
+            setName(view,"notready");
+        }
 
     }
 
-    private void launchToastService(View view){
-
-
+    public void setName(View view, String s){
+        TextView testView = (TextView)findViewById(R.id.areaName);
+        testView.setText(s);
     }
-
-    private void setText(){
-
-    }
-
 
 }
